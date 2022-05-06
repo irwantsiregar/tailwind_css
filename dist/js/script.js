ae7ab4e0fa51d08bfce7e00897a30efba3629c16
Sigadong <irwanto.regar30@gmail.com>
2022-05-06 23:50:54 +0700

    added features scroll-smooth in html and window click hamburger hidden in js

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function () {
   const header = document.querySelector('header');
   const fixedNav = header.offsetTop;
+  const toTop = document.querySelector('#to-top');
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add('navbar-fixed');
+    toTop.classList.remove('hidden');
+    toTop.classList.add('flex');
   } else {
     header.classList.remove('navbar-fixed');
+    toTop.classList.add('hidden');
+    toTop.classList.remove('flex');
   }
 }
 
@@ -17,4 +22,12 @@ const navMenu = document.querySelector('#nav-menu');
 hamburger.addEventListener(('click'), function () {
   hamburger.classList.toggle('hamburger-active');
   navMenu.classList.toggle('hidden');
+});
+
+// Klik diluar hamburger
+window.addEventListener('click', function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove('hamburger-active');
+    navMenu.classList.add('hidden');
+  }
 });
