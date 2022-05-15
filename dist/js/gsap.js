5efc2b738a109866147260444cfb65a238220cb7
Sigadong <irwanto.regar30@gmail.com>
2022-05-15 22:14:57 +0700

    update view image projects

--- dist/js/gsap.js
+++ dist/js/gsap.js
@@ -2,8 +2,8 @@ gsap.registerPlugin(TextPlugin);
 gsap.to('#myskill-1', {
   duration: 1.8,
   delay: 1.3,
-  text: "Cloud & Back-End Developer",
+  text: "Front-End | Back-End Developer",
 });
 
 gsap.from('header', { duration: 1.5, y: '-100%', ease: 'bounce', opacity: 0 });
-gsap.from('#say-hello', { duration: 1.5, x: '-100%', ease: 'bounce', opacity: 0 });
+gsap.from('#say-hello', { duration: 1.5, x: '100%', ease: 'bounce', opacity: 0 });
